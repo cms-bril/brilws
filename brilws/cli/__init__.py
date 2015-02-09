efce68ed335b2c3e784338ba7559c7dfa2f9cc14
Zhen Xie <zhen@pczhen.cern.ch>
2015-02-09 20:17:31 +0100

    rename cmmd

--- brilws/cli/__init__.py
+++ brilws/cli/__init__.py
@@ -4,5 +4,3 @@ from brilws.cli.briltag_main import briltag_main
 assert briltag_main
 from brilws.cli.brilschema_main import brilschema_main
 assert brilschema_main
-from brilws.cli.brildb_frommap import brildb_frommap
-assert brildb_frommap
