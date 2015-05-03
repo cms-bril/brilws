97650c34798b311107a2506ff260c32a0ab454dc
Zhen Xie <zhen@pczhen.cern.ch>
2015-05-03 16:38:03 +0200

    add beam queries

--- utils/fullquery.py
+++ utils/fullquery.py
@@ -11,16 +11,17 @@ from brilws import api
 import time
 import array
 import re
+from prettytable import PrettyTable
 
 dbtimefm = 'MM/DD/YY HH24:MI:SS.ff6'
 pydatetimefm = '%m/%d/%y %H:%M:%S.%f'
 
-def datatagList(connection,datatagnameid,schemaname=None,runmin=None,runmax=None,fillmin=None,fillmax=None,beamstatus=None,amodetag=None,targetegev=None):
+def datatagIter(engine,datatagnameid,schemaname=None,runmin=None,runmax=None,fillmin=None,fillmax=None,beamstatus=None,amodetag=None,targetegev=None,chunksize=9999):
     '''
-    output:{(fillnum,runnum,lsnum):datatagid}
+    output: iterator
     select fillnum,runnum,lsnum,DATATAGID from <schemaname>.IDS_DATATAG [where ]
     '''
-    q = '''select FILLNUM as fillnum, RUNNUM as runnum, LSNUM as lsnum, max(DATATAGID) as datatagid from IDS_DATATAG where DATATAGNAMEID<=:datatagnameid'''
+    q = '''select FILLNUM as fillnum, RUNNUM as runnum, LSNUM as lsnum, BEAMSTATUS as beamstatus, AMODETAG as amodetag, TARGETEGEV as targetegev, max(DATATAGID) as datatagid from IDS_DATATAG where DATATAGNAMEID<=:datatagnameid'''
     qCondition = ''
     qPieces = []
     binddict = {'datatagnameid':datatagnameid}
@@ -48,32 +49,39 @@ def datatagList(connection,datatagnameid,schemaname=None,runmin=None,runmax=None
     if qPieces:
         qCondition = ' and '.join([qCondition]+qPieces)
     q = q + qCondition +' group by RUNNUM, LSNUM'
-    out = []
-    with connection.begin() as trans:
-        result = connection.execute(q,binddict)
-        for row in result:
-            fillnum = row['fillnum']
-            runnum = row['runnum']
-            lsnum = row['lsnum']
-            datatagid = row['datatagid']
-            out.append([fillnum,runnum,lsnum,datatagid])
-    return out
+    result = pd.read_sql_query(q,engine,chunksize=chunksize,params=binddict,index_col='datatagid')   
+    return result
+ 
+def beamInfoIter(engine,datatagidmin,datatagidmax,schemaname=None,tablename=None,chunksize=9999,withBX=False):
+    '''
+    output: 
+    query: select egev,intensity1,intensity2 from BEAM_RUN1 where DATATAGID>=:datatagidmin and DATATAGID<=:datatagidmax 
 
-def runListByTime(connection,datatagnameid,schemaname=None,timestampsecmin=None,timestampsecmax=None):
-    '''
-    
-    '''
-    pass
+    withbxquery: select b.DATATAGID as datatagid, b.EGEV as egev, b.INTENSITY1 as intensity1, b.INTENSITY2 as intensity2, bx.BXIDX as bxidx, bx.BXINTENSITY1 as bxintensity1, bx.BXINTENSITY2 as bxintensity2, bx.ISCOLLIDING as iscolliding from BEAM_RUN1 b, BX_BEAM_RUN1 bx where b.DATATAGID=bx.DATATAGID and b.DATATAGID>=:datatagidmin and b.DATATAGID<=:datatagidmax 
 
-def datatagidForTag(connection,datatagnameid):
-    '''
-    select max(DATATAGID) from IDS_DATATAG where RUNNUM=193091 and LSNUM=1 and DATATAGNAMEID<=0;
-    '''
-    pass
+    '''    
+    q = '''select DATATAGID as datatagid, EGEV as egev, INTENSITY1 as intensity1, INTENSITY2 as intensity2 from BEAM_RUN1 where DATATAGID>=:datatagidmin and DATATAGID<=:datatagidmax'''
+    if withBX:
+        q = '''select b.DATATAGID as datatagid, b.EGEV as egev, b.INTENSITY1 as intensity1, b.INTENSITY2 as intensity2, bx.BXIDX as bxidx, bx.BXINTENSITY1 as bxintensity1, bx.BXINTENSITY2 as bxintensity2, bx.ISCOLLIDING as iscolliding from BEAM_RUN1 b, BX_BEAM_RUN1 bx where b.DATATAGID=bx.DATATAGID and b.DATATAGID>=:datatagidmin and b.DATATAGID<=:datatagidmax'''
+        
+    result = pd.read_sql_query(q,engine,chunksize=chunksize,params={'datatagidmin':datatagidmin,'datatagidmax':datatagidmax},index_col='datatagid')
+    return result
+
 
 if __name__=='__main__':
     dburl = 'sqlite:///test.db'
     dbengine = create_engine(dburl)
-    dbconnection = dbengine.connect()
-    runlist = datatagList(dbconnection,0,runmin=193091,runmax=193091)
-    print runlist
+    pd.options.display.float_format = '{0:.2f}'.format
+    for idchunk in datatagIter(dbengine,0,runmin=193091,runmax=193091,chunksize=10):
+        dataids = idchunk.index
+        for beaminfochunk in beamInfoIter(dbengine,dataids.min(),dataids.max(),chunksize=4000,withBX=False):
+            finalchunk = idchunk.join(beaminfochunk,how='inner',on=None,lsuffix='l',rsuffix='r',sort=False)            
+            print finalchunk
+            #x = PrettyTable(finalchunk.columns)
+            #for row in finalchunk.iterrows():
+            #    x.add_row(row)
+            #    x.align = 'r'
+            #print(x)
+            del beaminfochunk
+        del idchunk
+
