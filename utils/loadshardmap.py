6ea001c708af2ecf65d4eadda3a0fe1dbcfa68ab
Zhen Xie <zhen@pczhen.cern.ch>
2015-05-14 16:59:31 +0200

    split fixed and sharded schema

---
+++ utils/loadshardmap.py
@@ -0,0 +1,12 @@
+from sqlalchemy import *
+import pandas as pd
+if __name__=='__main__':    
+    resultcolumns = ['id','minrun','maxrun']
+    infilename = 'data/tableshards.csv'
+    fromfile = pd.read_csv(infilename,index_col=False)
+    fromfile.columns = resultcolumns
+    print fromfile.to_string(index=False)
+    destdbstr = 'sqlite:///test.db'
+    destengine = create_engine(destdbstr)
+    fromfile.to_sql('TABLESHARDS',destengine,if_exists='replace')
+    fromfile = pd.read_csv(infilename,index_col=False)
