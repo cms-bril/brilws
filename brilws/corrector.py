7d906bd5c4995ec4fcd92e5b4955602c5bf721ee
Zhen Xie <zhen@pczhen.cern.ch>
2015-08-13 15:53:42 +0200

    add afterglowstr

--- brilws/corrector.py
+++ brilws/corrector.py
@@ -1,8 +1,11 @@
+import ast
 import numpy as np
 from numpy.polynomial import Polynomial as P
 
 def apply_poly_str(ivalue,coefsstr,isreverse=False):
     '''
+    apply polynomial function on ivalue.
+    where the input coef is in string format. space separated float array or a single float 
     '''
     coefs = np.fromstring(coefsstr, dtype=np.float, sep=',')
     return apply_poly(ivalue,coefs=coefs,isreverse=isreverse)
@@ -16,6 +19,14 @@ def apply_poly(ivalue,coefs=[0,1],isreverse=False):
     if isreverse:
         return 1./result
     return result
+
+def apply_afterglow_str(ivalue,nbxs,afterglowthresholdsstr):
+    '''
+    apply afterglow correction on ivalue
+    where the input afterglow thresholds is in string format. space separated pair array
+    '''
+    afterglowthresholds = np.array(ast.literal_eval(afterglowthresholdsstr))
+    return apply_afterglow(ivalue,nbxs,afterglowthresholds)
     
 def apply_afterglow(ivalue,nbxs,afterglowthresholds):
     afterglow = 1.0
@@ -33,5 +44,7 @@ if __name__=='__main__':
     print xstr
     xstr = apply_poly_str(2.5,"0.,2.5",isreverse=False)
     print xstr
-        
+    astr = '(213,0.992),(321,0.99),(423,0.988)'
+    print apply_afterglow_str(1.,300,astr)
+    
         
