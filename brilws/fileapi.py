5ef24fc4c20f5332200c23c85c3602fe1f79002c
Zhen Xie <zhen@pczhen.dyndns.cern.ch>
2019-03-26 17:31:39 +0100

    add dataFilter

--- brilws/fileapi.py
+++ brilws/fileapi.py
@@ -1,13 +1,20 @@
 import tables as tb
 import numpy as np
 #import multiprocessing as mp
-#import pandas as pd
-import os
+import pandas as pd
+import os,sys
 import logging
 
 from time import time
 
 log = logging.getLogger('brilws')
+
+def _is_element_tuple(me):
+    '''
+    Check if element of me is tuple 
+    Input: collection
+    '''
+    return np.all([isinstance(x,tuple) for x in me])
 
 def _is_subset(me,target):
     '''
@@ -44,7 +51,13 @@ def open_validfiles(filenames,requiredtables):
     
 def dataRangeIter(filehandles,datatable,runmin=None,runmax=None,fillmin=None,tssecmin=None,tssecmax=None,fillmax=None):
     '''
-    Generate data coordinate
+    Iterator generating (tablehandle,data coordinate in files) based on range selection parameters
+    Input:
+        filehandles: input file handles
+        datatable: data table name without '/'
+        range selection parameters: runmin,runmax,fillmin,tssecmin,tssecmax,fillmax. None means select all        
+    Output: 
+       (tablehandle,data coordinate)
     '''
     conditions = []
     if runmin and runmax and runmin==runmax:
@@ -74,54 +87,103 @@ def dataRangeIter(filehandles,datatable,runmin=None,runmax=None,fillmin=None,tss
         if len(coordinates)==0:
             continue
         yield (n,coordinates) 
+    
 
-def dataFilter(tablehandle,coordinates,field_dtypes,runlsselectSeries=None):
+def dataFilter(tablehandle,coordinates,field_dtypes,runlsRangeSelectSeries=None,runlsnbPointFilterSeries=None):
     '''
-    Filter input table nodes on fields, runls select, bx select conditions
+    Filter input table nodes on fields based on runls range selection and runlsnb point selection conditions
     Input:  
             tablehandle: table handle
             coordinates: row coordinates
             fields: output fields dtype
-            runlsselect Series: runls selection matrix
+            runlsRangeSelectSeries: run,ls range selection matrix. Note: if None, select all
+            runlsnbPointSelectSeries: [run,(ls,nb)]. Note: if None, select all
     Output: 
-            numpy record array 
-    '''
-    result = None
+            numpy record array of type field_dtypes
+    Note: this filter does not produce None. Empty result means reject all
+    '''   
     myrows = tablehandle.read_coordinates(coordinates)
+    thisrun = np.array(myrows['runnum'])[0]
     lsnums = np.array(myrows['lsnum'])
-    if runlsselectSeries is not None:
-        selectedruns = runlsselectSeries.index.unique()
-        thisrun = np.array(myrows['runnum'])[0]
+    masks = np.full(lsnums.shape, True, dtype=bool) #default to select all
+
+    if runlsRangeSelectSeries is not None:
+        selectedruns = runlsRangeSelectSeries.index
         if thisrun not in selectedruns: #run is not selected
-            return result
-        thisselectedls = runlsselect[thisrun].values
-        masks = np.logical_or.reduce([np.logical_and(lsnums>=xmin, lsnums<=xmax) for [xmin,xmax] in thisselectedls]) #find masks
-    else:
-        masks = np.full(lsnums.shape, True, dtype=bool) #default to select all
-    nrows = masks[np.nonzero(masks)].size
-    log.debug('selected rows %d'%nrows)
+            masks = np.full(lsnums.shape, False, dtype=bool) #mask out all ls
+        else: 
+            thisselectedls = runlsselect[thisrun].values
+            masks = np.logical_or.reduce([np.logical_and(lsnums>=xmin, lsnums<=xmax) for [xmin,xmax] in thisselectedls]) #find masks   
+
+    if runlsnbPointFilterSeries is not None:
+        if thisrun not in runlsnbPointFilterSeries.index: #run is not selected
+            masks = np.full(lsnums[masks].shape, False, dtype=bool) #mask out all the rest ls                
+        else:
+            lsnbselect = runlsnbPointFilterSeries[thisrun].values.tolist() #this run values : [(lsnum,nbnum)] 
+            mylsnbs = [tuple(x) for x in np.column_stack((myrows['lsnum'][masks],myrows['nbnum'][masks]))]
+            for i,lsnb in enumerate(mylsnbs):
+                if lsnb not in lsnbselect:
+                    masks[i] = False
+
     all_fields = []
     for fieldname in field_dtypes.names:
         value = myrows[fieldname][masks]
         all_fields.append(value)
-    mytype = np.dtype(field_dtypes)
-    result = np.core.records.fromarrays(all_fields,names=field_dtypes.names,dtype=mytype)
+    nrows = masks[np.nonzero(masks)].size
+    log.debug('selected rows %d'%nrows)
+    result = np.core.records.fromarrays(all_fields,names=field_dtypes.names,dtype=field_dtypes)
     return result
 
+def _make_runlsnb_Series(runlsnbarray):
+    '''
+    Make pandas Series from [[run,ls,nb]] structure
+    Input: 2-D structure [[run,ls,nb]]
+    Output: pandas Series [(ls,nb), index=runs]
+    '''
+    runs = runlsnbarray[:,:1]
+    lsnb = [tuple(x) for x in runlsnbarray[:,1:3]]
+    return pd.Series(lsnb,index=runs.T[0])
+
 if __name__=='__main__':
-    tables = ['beam','hfetlumi']
+    tables = ['beam','tcds','hfetlumi']
     filenames = ['/home/zhen/data/7491/7491_327554_1812020507_1812020558.hd5','/home/zhen/data/7491/7491_327559_1812020558_1812020731.hd5','/home/zhen/data/7491/7491_327560_1812020731_1812021237.hd5']
     filehandles = open_validfiles(filenames,tables)
-    #print [x.filename for x in filehandles]
-    dataIter = dataRangeIter(filehandles,'hfetlumi',fillmin=7491,fillmax=7491)
-    then = time()
+    print [x.filename for x in filehandles]
+
+    tcdsIter = dataRangeIter(filehandles,'tcds',fillmin=7491,fillmax=7491)
+    
     #runlsselect = pd.Series([[1340,1345],[1400,1500]], index=[327554,327554]) #pd.Series , index=runnum, value=[[lsmin,lsmax]]
-    runlsselect = None
-    result_dtype = np.dtype( [('lsnum','uint32'),('timestampsec','uint32'),('avg','float32'),('avgraw','float32'),('bx','float32',(3564,)),('bxraw','float32',(3564,))] ) 
-    for (tablehandle,coordinates ) in dataIter: 
-        print coordinates
-        result = dataFilter(tablehandle,coordinates,field_dtypes=result_dtype,runlsselectSeries=runlsselect)
-        print result.shape
-        #print result[0]['lsnum'],result[0]['bx']
-    print time()-then
+    runlsselect = pd.Series([[1,10],[14,15]], index=[327560,327560]) #pd.Series , index=runnum, value=[[lsmin,lsmax]]
+    #runlsselect = None
+
+    tcds_result_dtype = np.dtype( [('runnum','uint32'),('lsnum','uint32'),('nbnum','uint32'),('timestampsec','uint32'),('cmson','bool8'),('deadfrac','float32'),('ncollidingbx','uint32')] )
+    tcds_result = None      
+    then = time()
+    for (tablehandle,coordinates ) in tcdsIter:         
+        tcds_result = dataFilter(tablehandle,coordinates,field_dtypes=tcds_result_dtype,runlsRangeSelectSeries=runlsselect)
+    print 'Time processing tcds: ',time()-then
+    tcdsrunlsnbSeries = None
+    if tcds_result.size :
+      selected_time = np.column_stack((tcds_result['runnum'],tcds_result['lsnum'],tcds_result['nbnum']))       
+      tcdsrunlsnbSeries = _make_runlsnb_Series( selected_time )
+      #selected_time = np.vstack(set(tuple(row) for row in selected_time))
+  
+    if tcdsrunlsnbSeries is None:
+        print 'no tcds selected'
+        sys.exit(0)
+
+    print 'tcds result ',tcds_result.shape
+    dataIter = dataRangeIter(filehandles,'hfetlumi',fillmin=7491,fillmax=7491)
+    hfetlumi_result_dtype = np.dtype( [('runnum','uint32'),('lsnum','uint32'),('nbnum','uint32'),('timestampsec','uint32'),('avg','float32'),('avgraw','float32'),('bx','float32',(3564,)),('bxraw','float32',(3564,))] ) 
+    hfetlumi_result = None
+    then = time()
+    for (tablehandle,coordinates) in dataIter: 
+        hfetlumi_result = dataFilter(tablehandle,coordinates,field_dtypes=hfetlumi_result_dtype,runlsRangeSelectSeries=None,runlsnbPointFilterSeries=tcdsrunlsnbSeries)
+        #print hfetlumi_result
+
+    print 'hfetlumi result ',hfetlumi_result.shape
+    print 'Time processing hfetlumi: ',time()-then
+    
+    #beam_result_dtype = np.dtype( [('lsnum','uint32'),('timestampsec','uint32'),('status','bool8')] )
+    #beam_result = None   
     [f.close() for f in filehandles]
