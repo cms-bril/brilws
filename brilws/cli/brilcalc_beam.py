0809fccef811e39a6e13753316d16c830d74a78f
Zhen Xie <Zhen.Xie@cern.ch>
2020-02-07 21:21:22 +0100

    fix item

--- brilws/cli/brilcalc_beam.py
+++ brilws/cli/brilcalc_beam.py
@@ -32,7 +32,7 @@ def validate(optdict):
     argdict = clicommonargs.argvalidators
     #extract sub argdict here
     myvalidables = ['-c','-n','-f','-r','-i','-o','--amodetag','-b','--beamenergy','--datatag','--begin','--end','--output-style',str]
-    argdict = dict((k,v) for k,v in clicommonargs.argvalidators.iteritems() if k in myvalidables)
+    argdict = dict((k,v) for k,v in clicommonargs.argvalidators.items() if k in myvalidables)
     schema = Schema(argdict)
     result = schema.validate(optdict)
     return result
