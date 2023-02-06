195f594f93e929b1c045de0001765fefcaf25e0f
Zhen Xie <zhen.xie@cern.ch>
2023-02-06 18:57:36 +0100

    add --dataset in validation list

--- brilws/cli/brilcalc_trg.py
+++ brilws/cli/brilcalc_trg.py
@@ -25,7 +25,7 @@ def validate(optdict):
     result={}
     argdict = clicommonargs.argvalidators
     #extract sub argdict here
-    myvalidables = ['-c','-r','--hltpath','--hltconfig',str]
+    myvalidables = ['-c','-r','--hltpath','--hltconfig','--dataset',str]
     argdict = dict((k,v) for k,v in clicommonargs.argvalidators.items() if k in myvalidables)
     schema = Schema(argdict)
     result = schema.validate(optdict)
