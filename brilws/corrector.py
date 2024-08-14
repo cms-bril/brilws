c20d0b26f447ae76ea1f829cabb8655dea69920e
Fabio Lucas Pereir Carneiro <fabio.carneiro@cern.ch>
2024-08-14 18:26:30 +0200

    Add new luminosity corrections. Available throught iovtag 'func' and 'payload' arguments.

--- brilws/corrector.py
+++ brilws/corrector.py
@@ -1,15 +1,22 @@
+from functools import wraps
+
 import ast
-import numpy as np
-import collections
 import copy
+import inspect
+import collections
+
 from numpy.polynomial import polynomial as P
+
+import numpy as np
+
 
 class FunctionRoot(object):
     def __init__(self,*args):
         self.root = []
         for a in args:
             self.root.append(a)
-            
+
+
 class CorrectionFunction(object):
     def __init__(self,fname,fparams):
         self.fname = fname
@@ -18,6 +25,34 @@ class CorrectionFunction(object):
         return self.fname
     def params(self):
         return self.fparams
+
+
+def collect_sanitizers(cls):
+    cls.required_params = {}
+    cls.validators = {}
+    for name, method in inspect.getmembers(cls, predicate=inspect.isfunction):
+        if hasattr(method, '_required_params') and hasattr(method, '_validators'):
+            cls.required_params[name] = method._required_params
+            cls.validators[name] = method._validators
+    return cls
+
+def argument_sanitizer(required_params, validators=None):
+    def decorator(func):
+        func._required_params = required_params
+        func._validators = validators if validators else []
+        @wraps(func)
+        def wrapper(instance, arg1, kwds):
+            cls = instance.__class__
+            cls.sanitize_arguments(kwds, func.__name__)
+            return func(instance, arg1, kwds)
+        return wrapper
+    return decorator
+
+def calculate_lumisection_sbil(ivalue, ncollidingbx):
+    with np.errstate(divide='ignore', invalid='ignore'):
+        ivalue = np.true_divide(ivalue, ncollidingbx)
+        ivalue = np.nan_to_num(ivalue, nan=0.0, posinf=0.0, neginf=0.0)
+    return ivalue
 
 def applyCorrectionStr(funcstrs , paramstrs, funcroot ):
         funcs_list = [ funcstrs.strip() ]
@@ -47,7 +82,7 @@ def applyCorrection(funcs, funcroot):
         withBX = True
         totlumi = fr.root[1]
         bxlumi = None
-        if not isinstance(toproot,collections.abc.Iterable) :
+        if not isinstance(toproot,collections.Iterable) :
             withBX = False
         else:
             bxlumi = toproot
@@ -60,8 +95,48 @@ def applyCorrection(funcs, funcroot):
                 bxlumi = FunctionCaller( f, FunctionRoot(bxlumi,totlumi,fr.root[2]) )
                 fr.root[0] = bxlumi
         return (totlumi,bxlumi)
-    
+
+@collect_sanitizers
 class FunctionFactory(object):
+    @staticmethod
+    def validate_required_arguments(params, func_name):
+        #TODO: Make this functions return a string option. Makes it easier to report all errors. If that is something desirable. May not be.
+        if not hasattr(FunctionFactory, func_name):
+            raise ValueError(f"Function '{func_name}' does not exist in class '{FunctionFactory.__name__}'.")
+        if func_name not in FunctionFactory.required_params:
+            raise ValueError(f"Function '{func_name}' cannot be used for corrections.")
+
+        # At this stage params will only be None if The iovtag
+        # provided no arguments under 'payload' This may be intended,
+        # if the func_name actually does not receive arguments.
+        # Otherwise it will be an improper iovtag.
+        if params is None:
+            params = {}
+
+        param_desc_list = FunctionFactory.required_params[func_name]
+        missing_params = []
+        for param, desc in param_desc_list:
+            if param not in params:
+                missing_params.append(f"- {param}: {desc}")
+        if missing_params:
+            err_str = (
+                f"The configuration for '{func_name}' is missing required arguments:\n"
+                + "\n".join(missing_params)
+            )
+            raise ValueError(err_str)
+
+        validators_list = FunctionFactory.validators[func_name]
+        validator_errs = []
+        for validator, err_msg in validators_list:
+            if not validator(params):
+                validator_errs.append(f"- {err_msg}")
+        if validator_errs:
+            err_str = (
+                f"The configuration for '{func_name}' has invalid parameters:\n"
+                + "\n".join(validator_errs)
+            )
+            raise ValueError(err_str)
+
     def hcaldrift(self,ivalue,rawavglumi,intglumi,nbx,a0=1.0,a1=0.0,a2=0.0,drift=0.0,c1=0.0,afterglowthresholds=''):
         '''
         this is a pain . just dump here. make it usable again only if later maybe used.        
@@ -76,7 +151,12 @@ class FunctionFactory(object):
         if afterglowthresholds:
             result = self.afterglow(result,nbx,afterglowthresholds)
         return result
-    
+
+    @argument_sanitizer(
+        required_params=(
+            ("coefs", "Comma-separated polynomial coefficients in decreasing order."),
+        )
+    )
     def poly1d(self,functionroot,kwds):
         '''
         root0: avglumi or bxlumi        
@@ -84,8 +164,8 @@ class FunctionFactory(object):
         ivalue = functionroot.root[0]
         ncollidingbx =  functionroot.root[2]
         coefsStr = kwds['coefs']
-        coefs = np.fromstring(coefsStr, dtype=np.float64, sep=',')
-        if not isinstance(ivalue,collections.abc.Iterable) : #is totallumi, in this case, need to use term totallumi/nbx
+        coefs = np.fromstring(coefsStr, dtype=np.float, sep=',')
+        if not isinstance(ivalue,collections.Iterable) : #is totallumi, in this case, need to use term totallumi/nbx
             with np.errstate(divide='ignore',invalid='ignore'):
                 ivalue = np.true_divide(ivalue,ncollidingbx)
                 if ivalue == np.inf:
@@ -93,11 +173,61 @@ class FunctionFactory(object):
                 ivalue = np.nan_to_num(ivalue)
         if len(coefs)>1:
             coefs = coefs[::-1] #reverse the order because polyval coefs order is reverse of np.poly1d
-        if isinstance(ivalue,collections.abc.Iterable) :
+        if isinstance(ivalue,collections.Iterable) :
             return P.polyval(ivalue,coefs)
         else:
-            return ncollidingbx*P.polyval(float(ivalue),coefs)           
-            
+            return ncollidingbx*P.polyval(ivalue,coefs)           
+
+    @argument_sanitizer(
+        required_params=(
+            ("sigvis", "Luminometer visible cross-section."),
+            ("eff", "Efficiency factor to be applied to 'sigvis'."),
+            ("slope", "Slope extracted from linear fit to sbil_1 / sbil_2 = a * sbil_2 + b."),
+            ("frev", "LHC's revolution frequency."),
+        )
+    )
+    def slopeApproximation(self, functionroot, kwds):
+        ivalue = functionroot.root[0]
+        ncollidingbx =  functionroot.root[2]
+
+        if isinstance(ivalue, collections.Iterable):
+            lumi = (kwds["frev"] * ivalue) / (kwds["sigvis"] * kwds["eff"])
+            return lumi - kwds["slope"] * lumi**2
+        else:
+            ivalue = calculate_lumisection_sbil(ivalue, ncollidingbx)
+            lumi = (kwds["frev"] * ivalue) / (kwds["sigvis"] * kwds["eff"])
+            return ncollidingbx * (lumi - kwds["slope"] * lumi**2)
+
+    @argument_sanitizer(
+        required_params=(
+            ("sigvis", "Luminometer visible cross-section."),
+            ("eff", "Efficiency factor to be applied to 'sigvis'."),
+            ("slope", "Slope extracted from linear fit to sbil_1 / sbil_2 = a * sbil_2 + b."),
+            ("intercept", "Intercept extracted from linear fit to sbil_1 / sbil_2 = a * sbil_2 + b."),
+            ("frev", "LHC's revolution frequency."),
+        ),
+        validators=(
+            (lambda kwds: not np.isclose(kwds["slope"], 0.0), "'slope' parameter can't be 0."),
+        )
+    )
+    def preciseCorrection(self, functionroot, kwds):
+        ivalue = functionroot.root[0]
+        ncollidingbx =  functionroot.root[2]
+
+        alpha = kwds["slope"] / kwds["intercept"] ** 2
+        if isinstance(ivalue, collections.Iterable):
+            lumi = (kwds["frev"] * ivalue) / (kwds["sigvis"] * kwds["eff"])
+            return (np.sqrt(4 * alpha * lumi + 1) - 1) / (2*alpha)
+        else:
+            ivalue = calculate_lumisection_sbil(ivalue, ncollidingbx)
+            lumi = (kwds["frev"] * ivalue) / (kwds["sigvis"] * kwds["eff"])
+            return ncollidingbx * (np.sqrt(4 * alpha * lumi + 1) - 1) / (2*alpha)
+
+    @argument_sanitizer(
+        required_params= (
+            ("coefs", "Literal python list containing the c_{i,j} coefs of the 2D polynomial"),
+        )
+    )
     def poly2dlL(self,functionroot,kwds):
         '''
         poly2d of l=bxlumi, L=totallumi
@@ -112,7 +242,7 @@ class FunctionFactory(object):
         totlumi = 0
         bxlumi = 0
 
-        if isinstance(l,collections.abc.Iterable):
+        if isinstance(l,collections.Iterable):
             bxlumi = np.array(l)
             totlumi = np.full_like(bxlumi, L)
         else:
@@ -128,15 +258,25 @@ class FunctionFactory(object):
        
         coefsStr = kwds['coefs']
 
-        coefs = np.array(ast.literal_eval(coefsStr), dtype=np.float64)
-        if isinstance(l,collections.abc.Iterable):
+        coefs = np.array(ast.literal_eval(coefsStr), dtype=np.float)
+        if isinstance(l,collections.Iterable):
             return P.polyval2d(bxlumi,totlumi,coefs)
         else:
-            return ncollidingbx*P.polyval2d(float(bxlumi),totlumi,coefs)
+            return ncollidingbx*P.polyval2d(bxlumi,totlumi,coefs)
 
+    @argument_sanitizer(
+        required_params=(
+            ("coefs", "Comma-separated polynomial coefficients in increasing order."),
+        )
+    )
     def inversepoly1d(self,functionroot,kwds):
         return 1./self.poly1d(functionroot,kwds)
     
+    @argument_sanitizer(
+        required_params=(
+            ("afterglowthresholds", "Pair or comma-separated list of pairslike (number_bxs, afterglow_factor)."),
+        )
+    )
     def afterglow(self,functionroot,kwds):
         ivalue = functionroot.root[0]
         ncollidingbx = functionroot.root[2]
@@ -152,12 +292,24 @@ class FunctionFactory(object):
 
         return ivalue*afterglow 
 
+    @argument_sanitizer(
+        required_params=(
+            ("coefs", "Comma-separated polynomial coefficients in decreasing order."),
+            ("afterglowthresholds", "Pair or comma-separated list of pairslike (number_bxs, afterglow_factor).")
+        )
+    )
     def poly1dWafterglow( self,functionroot,kwds ):
         poly1dresult = self.poly1d(functionroot,kwds)
         fr = FunctionRoot(poly1dresult,functionroot.root[1],functionroot.root[2])
         result = self.afterglow(fr,kwds)
         return result
 
+    @argument_sanitizer(
+        required_params=(
+            ("coefs", "Literal python list containing the c_{i,j} coefs of the 2D polynomial"),
+            ("afterglowthresholds", "Pair or comma-separated list of pairslike (number_bxs, afterglow_factor).")
+        )
+    )
     def afterglowWpoly2dlL(self,functionroot,kwds):
         '''
         afterglow*poly2dlL
@@ -170,6 +322,12 @@ class FunctionFactory(object):
         result = self.poly2dlL(fr,kwds)
         return result
 
+    @argument_sanitizer(
+        required_params=(
+            ("coefs", "Literal python list containing the c_{i,j} coefs of the 2D polynomial"),
+            ("afterglowthresholds", "Pair or comma-separated list of pairslike (number_bxs, afterglow_factor).")
+        )
+    )
     def poly2dlLWafterglow(self,functionroot,kwds):
         '''
         poly2dlL*afterglow
@@ -178,7 +336,8 @@ class FunctionFactory(object):
         fr = FunctionRoot(polyresult,functionroot.root[1],functionroot.root[2])
         result = self.afterglow(fr,kwds)
         return result
-    
+
+
 def FunctionCaller(func,functionroot):
     fac = FunctionFactory()
     try:
