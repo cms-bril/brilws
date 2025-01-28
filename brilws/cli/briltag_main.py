66ab4ca69a797c10e6d49719f9bb94c024e235c1
fabiocfabini <fabiolucas.carneiro@gmail.com>
2025-01-28 15:55:21 +0100

    Fix iovtag parsing bug. Raise version.

--- brilws/cli/briltag_main.py
+++ brilws/cli/briltag_main.py
@@ -32,7 +32,7 @@ def validate_iovdata(iovdata):
             func_name = data["func"]
             payload = data["payload"]
             try:
-                FunctionFactory.validate_required_arguments(payload, func_name)
+                FunctionFactory.sanitize_arguments(payload, func_name)
             except ValueError as err:
                 raise ValueError(f"Invalid config at run {run}.") from err
 
